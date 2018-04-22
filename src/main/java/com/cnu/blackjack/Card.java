779324e652d7aa709033f8d24935d5e3f8a97979
HanbumKo <hanbum.ko95@gmail.com>
2018-04-23 08:53:43 +0900

    Add string of card

--- src/main/java/com/cnu/blackjack/Card.java
+++ src/main/java/com/cnu/blackjack/Card.java
@@ -16,4 +16,33 @@ public class Card {
         this.rank = rank;
         this.suit = suit;
     }
+
+    public String getRankToString(Card card) {
+        switch( card.getRank() ) {
+            case 1: return "Rank : 1";
+            case 2: return "Rank : 2";
+            case 3: return "Rank : 3";
+            case 4: return "Rank : 4";
+            case 5: return "Rank : 5";
+            case 6: return "Rank : 6";
+            case 7: return "Rank : 7";
+            case 8: return "Rank : 8";
+            case 9: return "Rank : 9";
+            case 10: return "Rank : 10";
+            case 11: return "Rank : 11";
+            case 12: return "Rank : 12";
+            case 13: return "Rank : 13";
+            default: return "-----There is no appropriate rank number-----";
+        }
+    }
+
+    public String getSuitToString(Card card) {
+        switch( card.getSuit() ) {
+            case DIAMOND: return "Suit : DIAMOND";
+            case SPADE:   return "Suit : SPADE";
+            case CLUB:    return "Suit : CLUB";
+            case HEART:   return "Suit : HEART";
+            default:      return "-----There is no aapropriate suit type-----";
+        }
+    }
 }
