299c1ccba069d94c6a4465c299588fbdbd14fa03
HanbumKo <hanbum.ko95@gmail.com>
2018-04-23 13:40:32 +0900

    add comment

--- src/main/java/com/cnu/blackjack/Dealer.java
+++ src/main/java/com/cnu/blackjack/Dealer.java
@@ -9,6 +9,10 @@ public class Dealer {
 
     private Hand dealerHand;
 
+    /*
+    In contructor, first, dealer draw two cards from deck.
+    If score is less equal than 16, draw card until the score is higher than 16.
+     */
     public Dealer(Deck deck) {
         this.dealerHand = new Hand(deck);
         dealerHand.drawCard();
@@ -26,6 +30,10 @@ public class Dealer {
         return score;
     }
 
+    /*
+    Dealer must show a one card from dealerHand.
+    In this, show card index 0
+     */
     public void showOneCard() {
         System.out.println(dealerHand.getHandList().get(0).getRankToString() + "    " + dealerHand.getHandList().get(0).getSuitToString());
     }
