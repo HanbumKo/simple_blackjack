584a2424e8209363e9102e3545c4e6f30ec73164
HanbumKo <hanbum.ko95@gmail.com>
2018-04-23 13:30:01 +0900

    dealer

--- src/main/java/com/cnu/blackjack/Dealer.java
+++ src/main/java/com/cnu/blackjack/Dealer.java
@@ -1,11 +1,33 @@
 package com.cnu.blackjack;
 
+import lombok.Data;
+
 import java.util.concurrent.ThreadLocalRandom;
 
+@Data
 public class Dealer {
 
-    public int getScore() {
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        return random.nextInt(17, 25);
+    private Hand dealerHand;
+
+    public Dealer(Deck deck) {
+        this.dealerHand = new Hand(deck);
+        dealerHand.drawCard();
+        dealerHand.drawCard();
+        while( getScore()<=16 ) {
+            dealerHand.drawCard();
+        }
     }
+
+    public int getScore() {
+        int score = 0;
+        for( int i=0 ; i<dealerHand.getCurrentHandSize() ; i++ ) {
+            score += dealerHand.getHandList().get(i).getRank();
+        }
+        return score;
+    }
+
+    public void showOneCard() {
+        System.out.println(dealerHand.getHandList().get(0).getRankToString() + "    " + dealerHand.getHandList().get(0).getSuitToString());
+    }
+
 }
--- src/test/java/com/cnu/blackjack/DealerTest.java
+++ src/test/java/com/cnu/blackjack/DealerTest.java
@@ -8,8 +8,18 @@ public class DealerTest {
 
     @Test
     public void 딜러는_17에서_25사이의_점수를_반환한다() {
-        Dealer dealer = new Dealer();
+        Deck deck = new Deck(2);
+        Dealer dealer = new Dealer(deck);
         int score = dealer.getScore();
         assertTrue(score > 17 && score < 25);
     }
+
+    @Test
+    public void 딜러는_카드를_한장_보여준다() {
+        Deck deck = new Deck(2);
+        Dealer dealer = new Dealer(deck);
+        dealer.showOneCard();
+    }
+
+
 }
