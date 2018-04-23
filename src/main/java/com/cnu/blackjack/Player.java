9ec867957d65f10d1003b10f711098e7c08993d4
HanbumKo <hanbum.ko95@gmail.com>
2018-04-23 16:22:59 +0900

    temp save

--- src/main/java/com/cnu/blackjack/Player.java
+++ src/main/java/com/cnu/blackjack/Player.java
@@ -9,9 +9,12 @@ public class Player {
 
     private int balance;
     private int currentBet;
+    private Hand playerHand;
 
-    public Player(int balance) {
+    public Player(int balance, Deck deck) {
         this.balance = balance;
+        playerHand = new Hand(deck);
+
     }
 
     public void placeBet(int bet) {
@@ -20,5 +23,13 @@ public class Player {
         }
         balance = balance - bet;
         this.currentBet = bet;
+    }
+
+    public int getScore() {
+        int score = 0;
+        for( int i=0 ; i<playerHand.getCurrentHandSize() ; i++ ) {
+            score += playerHand.getHandList().get(i).getRank();
+        }
+        return score;
     }
 }
