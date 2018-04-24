cde643298e264e07ee382b60b12c57ecb28396c7
HanbumKo <hanbum.ko95@gmail.com>
2018-04-24 23:08:30 +0900

    fix conflict

--- src/test/java/com/cnu/blackjack/DealerTest.java
+++ src/test/java/com/cnu/blackjack/DealerTest.java
@@ -7,17 +7,25 @@ import static junit.framework.TestCase.assertTrue;
 public class DealerTest {
 
     @Test
-    public void 딜러는_17에서_25사이의_점수를_반환한다() {
+    public void 딜러는_17이상의_점수를_반환한다() {
         Deck deck = new Deck(2);
         Dealer dealer = new Dealer(deck);
+        dealer.getDealerHand().drawCard();
+        dealer.getDealerHand().drawCard();
+        while( dealer.getScore()<17 ) {
+            dealer.getDealerHand().drawCard();
+        }
+
         int score = dealer.getScore();
-        assertTrue(score > 17 && score < 25);
+        assertTrue(score >= 17);
     }
 
     @Test
     public void 딜러는_카드를_한장_보여준다() {
         Deck deck = new Deck(2);
         Dealer dealer = new Dealer(deck);
+        dealer.getDealerHand().drawCard();
+        dealer.getDealerHand().drawCard();
         dealer.showOneCard();
     }
 
