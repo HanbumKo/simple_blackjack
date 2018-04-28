98280d34f9e3beb9a9c5000bc879038edffca24a
Byeongman Lee <ssey0921@gmail.com>
2018-04-28 22:53:15 +0900

    Game 클래스 askPlayerHitOrStand 메소드 수정

--- src/test/java/com/cnu/blackjack/GameTest.java
+++ src/test/java/com/cnu/blackjack/GameTest.java
@@ -8,26 +8,45 @@ import static junit.framework.TestCase.assertTrue;
 public class GameTest {
 
     @Test
+    public void 게임을_기본으로_설정값은_플레이어_한명과_덱_한개이다() {
+        Game game = new Game();
+        int numberOfGamer = game.getNumberOfGamer();
+        int numberOfDeckOfCard = game.getDeck().getTotalCardCount();
+        assertTrue(numberOfGamer == 1);
+        assertTrue(numberOfDeckOfCard == 52); // 덱 1개는 52장의 카드를 가진다.
+    }
+
+    @Test
     public void 게임인원을_설정할수_있다() {
         Game game = new Game();
+        game.setNumberOfGamer(3);
+        int numberOfGamer = game.getNumberOfGamer();
+        assertTrue(numberOfGamer==3);
+    }
 
+    @Test
+    public void 게임인원을_플레이어리스트에_추가할수있다() {
+        Game game = new Game();
+        game.addGamer();
         int numberOfGamer = game.getNumberOfGamer();
         assertTrue(numberOfGamer==2);
     }
 
     @Test
-    public void 게임인원을_플레이어리스트에_추가할수있다() {
-        Game game = new Game(3, 2);
-        game.addGamer();
-        int numberOfGamer = game.getNumberOfGamer();
-        assertTrue(numberOfGamer==4);
-    }
-
-    @Test
     public void 게임진행() {
-        Game game = new Game(3, 2);
+        Game game = new Game();
         game.playBlackjack();
     }
 
 
+
+    @Test
+    public void 플레이어가_HIT를_하면_카드를_한장받는다(){
+        Game game = new Game();
+        int numberOfGamer = game.getNumberOfGamer();
+        for(int i = 0; i < numberOfGamer; i++ ) {
+            int HitOrStand = 1;
+            game.askPlayerHitOrStand(i, HitOrStand);
+        }
+    }
 }
